aaa505f444da4167803fc4058fa0b417d7772154
Млынник Семен <mlynnik@sfedu.ru>
2024-05-11 22:27:32 +0300

    Update Tasks.cs

--- CSharpHomework/Homework10/Tasks.cs
+++ CSharpHomework/Homework10/Tasks.cs
@@ -48,59 +48,60 @@ namespace Homework10
                     continue;
                 }
                 Console.Clear();
-        }
+            }
 
-        /// <summary>
-        /// Метод вызова второго модуля
-        /// </summary>
-        static void Task2()
-        {
-            WriteLine("Добро пожаловать в модуль \"Составитель вариантов\"!\nДоступные команды:\nСоздать\nВыход");
-            while (true)
+            /// <summary>
+            /// Метод вызова второго модуля
+            /// </summary>
+            static void Task2()
             {
-                Write("Введите команду: ");
-                var s = ReadLine();
-
-                if (s.ToLower() == "выход")
-                    return;
-
-                if (s.ToLower() == "создать")
+                WriteLine("Добро пожаловать в модуль \"Составитель вариантов\"!\nДоступные команды:\nСоздать\nВыход");
+                while (true)
                 {
-                    WriteLine("Добро пожаловать в создание вариантов!");
+                    Write("Введите команду: ");
+                    var s = ReadLine();
 
-                    var kr = new Generator();
-                    kr.LoadExercise();
+                    if (s.ToLower() == "выход")
+                        return;
 
-                    Write("Сколько вариантов вы хотите составить: ");
-                    var cnt_vars = int.Parse(ReadLine());
-
-                    while (true)
+                    if (s.ToLower() == "создать")
                     {
-                        Write("Сколько вопросов в каждом варианте вы хотите видеть: ");
-                        var cnt_exc = int.Parse(ReadLine());
-                        try
+                        WriteLine("Добро пожаловать в создание вариантов!");
+
+                        var kr = new Generator();
+                        kr.LoadExercise();
+
+                        Write("Сколько вариантов вы хотите составить: ");
+                        var cnt_vars = int.Parse(ReadLine());
+
+                        while (true)
                         {
-                            kr.GenerateVars(cnt_exc, cnt_vars);
-                            break;
+                            Write("Сколько вопросов в каждом варианте вы хотите видеть: ");
+                            var cnt_exc = int.Parse(ReadLine());
+                            try
+                            {
+                                kr.GenerateVars(cnt_exc, cnt_vars);
+                                break;
+                            }
+                            catch (ArgumentException e)
+                            {
+                                WriteLine($"Ошибка создания варианта: {e}");
+                            }
                         }
-                        catch (ArgumentException e)
-                        {
-                            WriteLine($"Ошибка создания варианта: {e}");
-                        }
+
+                        WriteLine("Варианты сгенерированны успешно!");
+                        WriteLine();
+
+                        Write("Как вы хотите назварь файлы с вариантами (Просто слово (Можно оставить пустым, тогда будет стандартное имя) без разрешения): ");
+                        var fname = ReadLine();
+                        kr.SaveVariants((string.IsNullOrWhiteSpace(fname) || fname == "") ? "Variant" : fname);
+
+                        WriteLine("Процесс сохранения вариантов завершён успешно!");
+                        WriteLine();
                     }
 
-                    WriteLine("Варианты сгенерированны успешно!");
-                    WriteLine();
-
-                    Write("Как вы хотите назварь файлы с вариантами (Просто слово (Можно оставить пустым, тогда будет стандартное имя) без разрешения): ");
-                    var fname = ReadLine();
-                    kr.SaveVariants((string.IsNullOrWhiteSpace(fname) || fname == "") ? "Variant" : fname);
-
-                    WriteLine("Процесс сохранения вариантов завершён успешно!");
-                    WriteLine();
+                    else WriteLine("Команда введена неверно! Попробуйте заново!");
                 }
-
-                else WriteLine("Команда введена неверно! Попробуйте заново!");
             }
         }
     }
